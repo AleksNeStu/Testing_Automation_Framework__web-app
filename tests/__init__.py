cd7b4a46691ebb657be51447b16e4669b8804052
AleksNeStu <a.v.nesterovich@gmail.com>
2016-09-30 12:51:32 +0300

    restructured

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Foobar.py: Description of what foobar does."""
+
+__author__ = 'AleksNeStu'
+__copyright__ = "The GNU General Public License v3.0"
--- tests/tasks/data/group.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-class Group:
-
-	def __init__(self, name, header, footer):
-		self.name = name
-		self.header = header
-		self.footer = footer
