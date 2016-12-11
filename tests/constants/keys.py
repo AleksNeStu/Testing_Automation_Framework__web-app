6a4ca2527e29bf89dbdc65a643f62d79e3cbad77
AleksNeStu <a.v.nesterovich@gmail.com>
2016-12-11 23:29:04 +0000

    - added new variables for dictionaries keys

---
+++ tests/constants/keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Dictionaries keys."""
+
+__author__ = 'AleksNeStu'
+__copyright__ = "The GNU General Public License v3.0"
+
+
+# Keys for contact's full name parts
+FIRST_NAME = "firstname"
+MIDDLE_NAME = "middlename"
+LAST_NAME = "lastname"
