7ee72084f0c325300b2aecbcd896f12b79e8f187
AleksNeStu <a.v.nesterovich@gmail.com>
2016-12-11 23:39:25 +0000

    - modified string operations, added regex approaches - added new methods

---
+++ tests/utils/strings.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Utils for make operations with strings (text)."""
+
+__author__ = 'AleksNeStu'
+__copyright__ = "The GNU General Public License v3.0"
+
+import re
+
+from tests.constants import data, keys
+
+
+def normal_select_title(ext_title):
+    """Normalize extended title (exclude brackets).
+    Example:
+    'Select (title_name)' to 'title_name'
+    """
+    return ext_title[ext_title.find("(")+1:ext_title.find(")")]
+
+def home_full_name_to_dict(full_name):
+    """Convert full name text line to dictionary with predefined keys
+    for home page.
+    Example:
+    'middle last first' to
+    {'firstname': 'first', 'middlename': 'middle', 'lastname': 'last'}
+    """
+    _full_name = full_name.split()
+    full_name_list = []
+    for _name_part in _full_name:
+        if data.CONTACT_FIRST_NAME in _name_part:
+            full_name_list.append((keys.FIRST_NAME, _name_part))
+        if data.CONTACT_MIDDLE_NAME in _name_part:
+            full_name_list.append((keys.MIDDLE_NAME, _name_part))
+        if data.CONTACT_LAST_NAME in _name_part:
+            full_name_list.append((keys.LAST_NAME, _name_part))
+    return dict(full_name_list)
+
+def home_all_phones_to_list(all_phones):
+    """Convert all phones multi lines to list for home page.
+    Example:
+    'home\nmobile\nwork\nsecondary\n' to ['home', 'mobile', 'work', 'secondary']
+    """
+    _all_phones = all_phones.splitlines()
+    if len(_all_phones) == 4: return _all_phones
+    else: return [None, None, None, None]
+
+def clean_phone(phone_number):
+    """Clean unnecessary parts from phone number text (exclude: '-', '(', ')', ' ')
+    Example:
+    '+1-377-368-4628' to '+13773684628'
+    """
+    return re.sub("[() -]","",phone_number)
