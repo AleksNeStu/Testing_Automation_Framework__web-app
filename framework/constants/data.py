e16f0214665b420c00b8234d6219363f8dfc18ed
AleksNeStu <a.v.nesterovich@gmail.com>
2016-12-16 18:12:50 +0000

    - deleted deprecated data

--- framework/constants/data.py
+++ framework/constants/data.py
@@ -1,39 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-"""Data for filling."""
-
+"""Modules for providing the web-app's data for filling UI fields."""
 __author__ = 'AleksNeStu'
 __copyright__ = "The GNU General Public License v3.0"
 
-
-# login credentials
-LOGIN = "admin"
-PASSWORD = "secret"
-
-# prefix for new objects
-_NEW = "New_"
 
 # data for creation (modification) contacts
 CONTACT_FIRST_NAME = "Contact_firstname_"
 CONTACT_MIDDLE_NAME = "Contact_middlename_"
 CONTACT_LAST_NAME = "Contact_lastname_"
 CONTACT_ADDRESS = "Contact_address_"
-CONTACT_EMAIL = "@gmail.com"
-
-CONTACT_FIRST_NAME_NEW = _NEW + CONTACT_FIRST_NAME
-CONTACT_MIDDLE_NAME_NEW = _NEW + CONTACT_MIDDLE_NAME
-CONTACT_LAST_NAME_NEW = _NEW + CONTACT_LAST_NAME
-CONTACT_ADDRESS_NEW = _NEW + CONTACT_ADDRESS
-CONTACT_EMAIL_NEW = _NEW + CONTACT_EMAIL
+CONTACT_EMAIL_DOMAIN = "@gmail.com"
 
 # data for creation (modification) groups
 GROUP_NAME = "Group_name_"
 GROUP_HEADER = "Group_header_"
 GROUP_FOOTER = "Group_footer_"
-GROUP_NAME_NEW = _NEW + GROUP_NAME
-GROUP_HEADER_NEW = _NEW + GROUP_HEADER
-GROUP_FOOTER_NEW = _NEW + GROUP_FOOTER
 
 # phone country code
 PHONE_CODE = 1
